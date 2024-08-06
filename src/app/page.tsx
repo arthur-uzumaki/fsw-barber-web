f5e4643c5a15f6725fa2400057a61768764f26f9
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-08-05 21:04:16 -0300

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import { Header } from "@/components/header"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="flex items-center p-4">
-      <Button>Teste</Button>
-    </div>
+    <main>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Arthur</h2>
+        <p>Segundo-feira, 05</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button size={"icon"}>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Banner-01"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </main>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+export function Header() {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image
+          src="/logo.png"
+          width={120}
+          height={18}
+          alt="FSW BARBER"
+          quality={100}
+        />
+
+        <Button size={"icon"} variant={"outline"}>
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
