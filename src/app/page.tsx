ee89c52cffa95b2dd181b90543a823e876d5d200
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-08-06 17:15:50 -0300

    feat: add  booking-item component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/components/header"
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
-import { Badge } from "@/components/ui/badge"
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
@@ -10,6 +9,8 @@ import Image from "next/image"
 import BarberShopItem, {
   BarberShopItemProps,
 } from "@/components/barber-shop-item"
+import { quickSearchOptions } from "./_constant/search"
+import { BookingItem } from "@/components/booking-item"
 
 async function fetchBarberShops() {
   const barbershops = await api("/barbershops")
@@ -36,45 +37,17 @@ export default async function Home() {
         </div>
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/cabelo.svg" alt="Cabelo" width={16} height={16} />
-            Cabelo
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/barba.svg" alt="Barba" width={16} height={16} />
-            Barba
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/acabamento.svg"
-              alt="Acabamento"
-              width={16}
-              height={16}
-            />
-            Acabamento
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/hidratacao.svg"
-              alt="Hidratação"
-              width={16}
-              height={16}
-            />
-            Hidratação
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/sobrancelha.svg"
-              alt="Sobrancelha"
-              width={16}
-              height={16}
-            />
-            Sobrancelha
-          </Button>
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/massagem.svg" alt="Massagem" width={16} height={16} />
-            Massagem
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} className="gap-2" variant={"secondary"}>
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
@@ -86,27 +59,7 @@ export default async function Home() {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-sm text-gray-400">AGENDAMENTOS</h2>
-        <Card className="">
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Vintage Barber</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2xl">06</p>
-              <p className="text-sm">09:45</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-sm text-gray-400">RECOMENDADOS</h2>
 
---
+++ src/components/booking-item.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card"
+import { Badge } from "./ui/badge"
+import { Avatar, AvatarImage } from "./ui/avatar"
+
+export function BookingItem() {
+  return (
+    <>
+      <h2 className="mb-3 mt-6 text-sm text-gray-400">AGENDAMENTOS</h2>
+      <Card className="">
+        <CardContent className="flex justify-between p-0">
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Vintage Barber</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Fevereiro</p>
+            <p className="text-2xl">06</p>
+            <p className="text-sm">09:45</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
